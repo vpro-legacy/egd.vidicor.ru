0e740eab82f6c1c71b87e9e362271bece9fc362e
vpro-legacy <vpro-legacy@users.noreply.github.com>
2018-06-16 23:46:22 +0300

    Data imported from web.archive.org

---
+++ feedback/index.php
@@ -0,0 +1,59 @@
+﻿<html>
+<head>
+<meta http-equiv="PRAGMA"   content="NO-CACHE" /> 
+<meta http-equiv="Cache-Control"  content="no-cache" />
+<meta http-equiv="Content-Type" Content-type: text/html; charset="utf-8" />
+<link type='image/x-icon' rel="shortcut icon" href="../../favicon.ico"> 
+<script language="javascript" src="http://plugin.vidicor.ru/vidicor.js"></script>
+<script type="text/javascript" src="../embed/embed.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+
+</head>
+
+<body>
+
+<center>        
+<table width="100%"  >
+  <tr>
+    <td>
+      <a href="http://fbtest.vidicor.ru/Help_Feedback.php" target="_blank">
+      <h2 style="text-transform:uppercase; font-size:18px; color:#d71732;">Инструкция &laquo;Как сделать звонок в студию&raquo;</h2>
+      </a>
+    </td>
+  <td>
+  <h3 style="font-size:17px; color: #231362; font-weight:bold; padding:0; margin:0;">УГЛТУ</h3>
+  </td>
+    
+    <td align="right">
+      <a href="http://bravica.net/ru/" target="_blank">
+      <h2 style="text-transform:uppercase; font-size:18px; color:#d71732;">Измерить скорость интернет-подключения</h2>
+      </a>
+    </td>
+</tr>
+</table>
+
+<script language="javascript" type="text/javascript">
+    vidicorplayer("100%", "100%", 
+    { 
+         url: "rmxvp://192.168.34.67/capture/livecam",
+                  buffer: 1000,
+                  EnableAudio:1,
+                  EnableLimit:0,
+                  ShowControls:1,
+                  EnableClientFeedback:1,
+                  SelectedVideoStream:1,
+                  ClientFeedbackAddr:"192.168.34.67:9005",
+                  ClientFeedbackChannels:1,
+                  ClientFeedbackBitrate:5000,
+                  ClientFeedbackConnectTimeoutMs:3000,
+                  clientfeedbackproto:"TCP"
+
+    }
+    )
+  </script>
+</center>
+
+ <center><IFRAME src='http://chat.vidicor.ru#Gitary' scrolling="0" width='100%' height='100%'></IFRAME></center>
+ 
